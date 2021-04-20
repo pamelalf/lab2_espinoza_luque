af1e57c399dd171d0e45618469d683f0cc27c345
pamelalf <69698365+pamelalf@users.noreply.github.com>
2021-04-19 19:41:29 -0500

    Add files via upload

---
+++ pucp/src/main/java/com/example/pucp/PucpApplication.java
@@ -0,0 +1,13 @@
+package com.example.pucp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PucpApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PucpApplication.class, args);
+	}
+
+}
---
+++ pucp/src/test/java/com/example/pucp/PucpApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.pucp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PucpApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
